61ad322f3f74b399d9393e00f48a23366b2ee3ed
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-03-20 17:57:21 +0900

    Todo HTML $ Write HTML 생성

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require(`express`);
 const app = express();
+const bodyParser = require(`body-parser`);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(8080, function () {
   console.log(`Hello`);
@@ -7,4 +9,13 @@ app.listen(8080, function () {
 
 app.get(`/`, function (req, res) {
   res.sendFile(__dirname + `/index.html`);
+});
+
+app.get(`/write`, function (요청, 응답) {
+  응답.sendFile(__dirname + `/write.html`);
+});
+
+app.post(`/add`, function (요청, 응답) {
+  응답.send(`전송완료`);
+  console.log(요청.body);
 });
